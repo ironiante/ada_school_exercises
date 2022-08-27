207357851be7a0346a4d8aa0899f60d41ba7cd57
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-08-27 18:28:22 -0500

    ada school exercises

---
+++ src/funciones/Gato.java
@@ -0,0 +1,14 @@
+package funciones;
+
+public class Gato {
+    public static void main(String[] args) {
+        Gato miau1=new Gato();
+        miau1.ronronea();
+
+    }
+    //metodo
+    void ronronea(){
+        System.out.println("ruuuuurooo");
+
+    }
+}
