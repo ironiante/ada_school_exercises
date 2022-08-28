e387426fab55c719b365deacb4de651ae3284125
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-08-28 15:09:15 -0500

    exercise functions

--- src/funciones/Gato.java
+++ src/funciones/Gato.java
@@ -1,5 +1,9 @@
 package funciones;
-
+/**
+ * Utilizando Replit Crea una clase con un metodo main, luego crea un metodo que no
+ * retorne valor, donde realice la impresión por la interfaz consola de un mensaje,
+ * posterior a eso llama a dicho nuevo metodo en el metodo main.
+ */
 public class Gato {
     public static void main(String[] args) {
         Gato miau1=new Gato();
